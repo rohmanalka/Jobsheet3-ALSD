b12ed8f88ac760a849d567c5b81f8d6aa3ed2e8a
rohmanalka <rohmanalka06@gmail.com>
2024-02-29 09:43:50 +0700

    3.4 konstruktor parameter + 2 method

--- PersegiPanjang.java
+++ PersegiPanjang.java
@@ -1,4 +1,24 @@
 public class PersegiPanjang {
     public int panjang;
     public int lebar;
+
+    public PersegiPanjang(int p, int l) {
+        panjang = p;
+        lebar = l;
+    }
+
+    void cetakInfo() {
+        System.out.println("panjang: " + panjang + " lebar: " + lebar +
+        " Luas: " + hitungLuas(panjang, lebar) + " Keliling: " + hitungKeliling(panjang, lebar));
+    }
+
+    int hitungLuas (int p, int l) {
+        int Luas = p * l;
+        return Luas;
+    }
+
+    int hitungKeliling (int p, int l) {
+        int Keliling = 2 * (p+l);
+        return Keliling;
+    }
 }

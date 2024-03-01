c82c80cf83d9af1b64157698f6149f2e25d32dfa
rohmanalka <rohmanalka06@gmail.com>
2024-03-01 18:42:37 +0700

    Teori

---
+++ CanteenDemo21.java
@@ -0,0 +1,20 @@
+public class CanteenDemo21 {
+    public static void main(String[] args) {
+        Item21 item1 = new Item21();
+        item1.name = "Nasi Kebuli";
+        item1.price = 10000;
+        item1.stock = 10;
+
+        Item21 item2 = new Item21();
+        item2.name = "Risoles";
+        item2.price = 2000;
+        item2.stock = 20;
+
+        Item21 item3 = new Item21();
+        item3.name = "Ultramilk";
+        item3.price = 5000;
+        item3.stock = 15;
+
+    }
+
+}
